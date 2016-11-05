5dd2954c86275de6d27d4214b8e859fe68020b8c
Norman Joyner <norman.joyner@gmail.com>
2016-11-04 21:48:26 -0400

    Move application.js to index.js and es6-ify

---
+++ index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+class MemoryLeakHandler {
+
+    constructor(options) {
+        if(!options) {
+            options = {};
+        }
+
+        if(options.autostart === undefined) {
+            options.autostart = true;
+        }
+
+        this.max_rss = options.max_rss || 128;
+        this.check_interval = options.check_interval || 1000;
+
+        if(options.on_exit && typeof options.on_exit == 'function') {
+            this.on_exit = options.on_exit;
+        }
+
+        if(options.autostart) {
+            this.start();
+        }
+    }
+
+    start() {
+        this.interval = setInterval(() => {
+            const rss = process.memoryUsage().rss / (1024 * 1024);
+            if(rss > this.max_rss) {
+                this.on_exit(() => {
+                    process.exit(1);
+                });
+            }
+        }, this.check_interval);
+    }
+
+    stop() {
+        clearInterval(this.interval);
+    }
+
+    on_exit(callback) {
+        return callback();
+    }
+
+}
+
+module.exports = MemoryLeakHandler;
